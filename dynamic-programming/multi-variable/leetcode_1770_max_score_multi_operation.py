31aa92fe34160b8004c2e6fe2e41d7ad03c3e24c
Gurminder <gurminder95@gmail.com>
2022-02-20 19:40:25 -0400

    leetcode#1770

---
+++ dynamic-programming/multi-variable/leetcode_1770_max_score_multi_operation.py
@@ -0,0 +1,23 @@
+# left -> index of the last number picked from the left side of the array, nums.
+# i -> index of the last multiplier picked from the multiplier array.
+# (i - left) -> number of elements picked from the right side of array.
+# right -> index of the last number picked from the right side of the array, nums. n - (i-left) -1
+
+
+def maximumScore(nums: List[int], multipliers: List[int]) -> int:
+    
+    n = len(nums)
+    m = len(multipliers)
+
+    def dp(i, left):
+        # Base case
+        if i == m:
+            return 0
+        
+        right = n - (i-left) - 1
+
+        return max(nums[left]*multipliers[i] + dp(i+1, left+1), nums[right]*multipliers[i] + dp(i+1, left))
+    return dp(0, 0)
+
+
+print(maximumScore([1,2,3], [3,2,1]))
