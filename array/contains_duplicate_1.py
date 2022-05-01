9caf3136aa0bc7faf5732ba8fbe11266d23efab4
Gurminder <gurminder95@gmail.com>
2022-05-01 15:54:29 -0300

    adding leetcode#217

---
+++ array/contains_duplicate_1.py
@@ -0,0 +1,10 @@
+def containsDuplicate(self, nums: List[int]) -> bool:
+    hashset = set()
+    for num in nums:
+        # Time complexity is O(1)
+        # https://wiki.python.org/moin/TimeComplexity
+        if num in hashset:
+            return True
+        else:
+            hashset.add(num)
+    return False
