fe9e8cf420bff5318c83230405888e895dec7f3c
Gurminder <gurminder95@gmail.com>
2022-05-01 23:25:16 -0300

    Add valid anagram soln

---
+++ array/anagram.py
@@ -0,0 +1,19 @@
+# Check if a string is a valid anagram of another.
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+# Leetcode#242
+
+def isAnagram(self, s: str, t: str) -> bool:
+    alphabet_array = [0] * 26
+    
+    if len(s) != len(t):
+        return False
+    
+    for i in range(len(s)):
+        alphabet_array[ord(s[i]) - ord('a')] += 1
+        alphabet_array[ord(t[i]) - ord('a')] -= 1
+        
+    for count in alphabet_array:
+        if count is not 0:
+            return False
+    
+    return True
