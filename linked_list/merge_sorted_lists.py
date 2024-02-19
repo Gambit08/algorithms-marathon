7b50d2cc0bf2784230b5f58816b752d2c2da899c
Gurminder Parmar <gurminder95@gmail.com>
2024-02-19 14:41:10 -0400

    Merge two sorted lists by recursion

--- linked_list/merge_sorted_lists.py
+++ linked_list/merge_sorted_lists.py
@@ -9,7 +9,7 @@ class ListNode:
 # Time Complexity = O(n+m) where n & m are the no. of nodes in list1 and list2 respectively.
 # Space Complexity = O(1)
 
-def merge_sorted_lists(list1, list2):
+def merge_sorted_lists(list1:ListNode, list2:ListNode):
     dummy = ListNode()
     tail = dummy
 
@@ -34,3 +34,24 @@ def merge_sorted_lists(list1, list2):
         tail.next = list1
     
     return dummy.next
+
+# Recursion
+
+def _merge_sorted_lists(list1:ListNode, list2:ListNode):
+
+    if not list1:
+        return list2
+    
+    if not list2:
+        return list1
+
+    if list1.val < list2.val:
+
+        list1.next = _merge_sorted_lists(list1.next, list2)
+        return list1
+    
+    else:
+
+        list2.next = _merge_sorted_lists(list1, list2.next)
+        return list2
+    
